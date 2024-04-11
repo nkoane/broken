f9118a50b2478a22a73673b482660b687ce66bdf
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-04-11 15:42:59 +0200

    lesson 1, done.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/bio', function () {
+    return view('bio');
+});
+
+Route::get('/posts', function () {
+    return ['posts' => ['an Alter Native.', 'an Alter Native.']];
+});
