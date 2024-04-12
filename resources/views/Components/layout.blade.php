5e6f13a712975e8ef78468a8a902e4eadd950242
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-04-12 09:27:47 +0200

    prettier integrated.

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -1,27 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>BROK/N</title>
+        @vite('resources/css/app.css')
+    </head>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BROK/N</title>
-    @vite('resources/css/app.css')
-</head>
-
-<body class="bg-gray-50 flex flex-col h-screen">
-    <header class="flex justify-between items-center p-4">
-        <h1 class="text-3xl font-bold transition-all duration-500 hover:text-yellow-600"><a href="/">BROK+N</a></h1>
-        <nav class="flex gap-2">
-            <x-nav.link href="/bio" label="biography" />
-            <x-nav.link href="/tags" label="tags" />
-        </nav>
-    </header>
-    <main class="flex-grow bg-yellow-50 p-4">
-        {{ $slot }}
-    </main>
-    <footer class="bg-gray-100 text-xs text-left px-2 py-1">
-        <p>ETSPx &copy; {{ date('Y') }}</p>
-    </footer>
-</body>
-
+    <body class="flex h-screen flex-col bg-gray-50">
+        <header class="flex items-center justify-between border-b-2 border-gray-100 bg-gradient-to-b from-gray-50 to-yellow-50 p-4">
+            <h1 class="text-3xl font-bold transition-all duration-500 hover:text-yellow-600"><a href="/">BROK+N</a></h1>
+            <nav class="flex gap-2">
+                <x-nav.link href="/bio" label="biography" />
+                <x-nav.link href="/tags" label="tags" />
+            </nav>
+        </header>
+        <main class="flex-grow bg-yellow-50 p-4">
+            {{ $slot }}
+        </main>
+        <footer class="bg-gray-100 px-2 py-1 text-left text-xs">
+            <p>ETSPx &copy; {{ date('Y') }}</p>
+        </footer>
+    </body>
 </html>
--- resources/views/Components/nav/link.blade.php
+++ resources/views/Components/nav/link.blade.php
@@ -1,4 +1,3 @@
-<a href="{{ $href }}"
-    class="transition-all duration-500 block px-1 font-bold text-yellow-600  hover:bg-yellow-300 hover:text-black">
+<a href="{{ $href }}" class="block px-1 font-bold text-yellow-600 transition-all duration-500 hover:bg-yellow-300 hover:text-black">
     {{ $label }}
 </a>
