1f666de8f09daa1a0405f1c312d4584033dbf940
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-04-19 17:18:48 +0200

    Update layout.blade.php to include BROK+N logo with hover effect

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -9,7 +9,13 @@
 
     <body class="flex h-screen flex-col bg-gray-50">
         <header class="flex items-center justify-between border-b-2 border-gray-100 bg-gradient-to-b from-gray-50 to-yellow-50 p-4">
-            <h1 class="text-3xl font-bold transition-all duration-500 hover:text-yellow-600"><a href="/">BROK+N</a></h1>
+            <h1 class="text-3xl font-bold transition-all duration-500 hover:text-yellow-600">
+                <a href="/" class="group flex items-center">
+                    <span>BROK</span>
+                    <span class="transition-color duration-500 group-hover:bg-black group-hover:text-white">+</span>
+                    <span>N</span>
+
+            </h1>
             <nav class="flex gap-2">
                 <x-nav.link href="/bio" label="biography" />
                 <x-nav.link href="/tags" label="tags" />
@@ -18,7 +24,7 @@
         <main class="flex-grow bg-yellow-50 p-4">
             {{ $slot }}
         </main>
-        <footer class="bg-gray-100 px-2 py-1 text-left text-xs">
+        <footer class="bg-gray-100 px-2 py-1 text-right text-xs">
             <p>ETSPx &copy; {{ date('Y') }}</p>
         </footer>
     </body>
