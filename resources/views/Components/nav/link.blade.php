e87a3f1a5619cc2e2c400300065ee044486b5006
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-05-12 10:39:02 +0200

    okay, whatever, i am hungry.

--- resources/views/Components/nav/link.blade.php
+++ resources/views/Components/nav/link.blade.php
@@ -1,9 +1,9 @@
 @props([
-    'active',
+    'active' => false,
 ])
 <a
     {{ $attributes }}
     aria-current="{{ $active ? 'active' : 'false' }}"
-    class="{{ $active ? 'outline outline-yellow-400' : '' }} px-2 py-1">
+    class="{{ $active ? ' outline outline-black ' : '' }} px-2 py-1 transition-colors duration-75">
     {{ $slot }}
 </a>
