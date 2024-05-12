b767b57a46ff90a67beda73da52e2eac607305f9
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-05-12 09:52:52 +0200

    added route names

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('root');
-});
+})->name('root');
 
 Route::get('/bio', function () {
     return view('bio');
-});
+})->name('bio');
 
 Route::get('/work', function () {
     return view('work');
-});
+})->name('work.index');
