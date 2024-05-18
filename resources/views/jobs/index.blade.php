aec42aa4e1a0b3fb23a4be19f12063b9abe2cb00
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-05-18 13:59:25 +0200

    just added the some css styling to job listing just learning vim/neovim

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -4,15 +4,15 @@
     @if (! empty($jobs))
         <ol class="flex flex-wrap gap-2">
             @foreach ($jobs as $job)
-                <li class="w-1/4 flex-grow bg-gray-50 p-4">
-                    <dl class="w-full">
+                <li class="w-1/4 flex-grow bg-gray-50 p-4 py-2 even:bg-blue-100 hover:bg-slate-100">
+                    <dl class="flex h-full w-full flex-col">
                         <dt class="mb-2 text-xl font-bold">
                             <a class="text-blue-600 hover:underline" href="{{ route('jobs.job.show', $job->id) }}">{{ $job['title'] }}</a>
                         </dt>
-                        <dd>
+                        <dd class="g-yellow-100 flex-grow pb-1">
                             {{ $job->description }}
                         </dd>
-                        <dd>
+                        <dd class="border-t pt-2">
                             <strong>ZAR</strong>
                             {{ number_format($job->salary, 0, '.', ' ') }}
                         </dd>
