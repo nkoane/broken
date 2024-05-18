549f44193703b010a4ad159b7d22976c86e066e2
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-05-18 18:03:10 +0200

    added relationship between job and employer (belongsTo and hasMany)

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Job;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employer extends Model
 {
     use HasFactory;
+
+    public function jobs (): HasMany {
+        return $this->hasMany(Job::class);
+    }
 }
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Employer;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
@@ -12,4 +14,8 @@ class Job extends Model
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'description', 'salary'];
+
+    public function employer(): BelongsTo {
+        return $this->belongsTo(Employer::class);
+    }
 }
