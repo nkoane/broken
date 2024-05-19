b6e0c8e79429b8c51b0a52ed2e94374efdaeed0d
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-05-19 10:05:57 +0200

    okay, added the employer/tag filtering

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,12 +1,23 @@
 <x-layout heading="jobs">
     <div class="mb-4 flex items-center justify-between">
-        <h2 class="text-4xl font-bold">Jobs</h2>
+        <div class="flex items-center gap-2">
+            <h2 class="text-4xl font-bold">jobs</h2>
+            @isset($tag)
+                /
+                <h2 class="text-3xl">{{ $tag->name }}</h2>
+            @endif
+
+            @isset($employer)
+                /
+                <h2 class="text-3xl">{{ $employer->name }}</h2>
+            @endif
+        </div>
         <div class="">
             {{ $jobs->links() }}
         </div>
     </div>
 
-    @if ($jobs->hasPages())
+    @if ($jobs->count() > 0)
         <ol class="flex flex-wrap gap-2">
             @foreach ($jobs as $job)
                 <li class="w-1/4 max-w-[33%] flex-grow bg-gray-50 p-4 py-2 even:bg-orange-100 hover:bg-slate-100">
@@ -17,9 +28,15 @@
                             </a>
                         </dt>
                         <dd class="text-md my-1 border-b border-t border-gray-400 pb-1 font-bold">
-                            <a class="text-blue-500 hover:underline" href="/jobs&employer={{ $job->employer->id }}">
-                                {{ $job->employer->name }}
-                            </a>
+                            @isset($employer)
+                                <a class="text-blue-500 hover:underline" href="{{ route('jobs.employer', $employer->id) }}">
+                                    {{ $employer->name }}
+                                </a>
+                            @else
+                                <a class="text-blue-500 hover:underline" href="{{ route('jobs.employer', $job->employer->id) }}">
+                                    {{ $job->employer->name }}
+                                </a>
+                            @endisset
                         </dd>
                         <dd class="g-yellow-100 flex-grow pb-1">
                             {{ $job->description }}
@@ -27,7 +44,7 @@
                         @if ($job->tags)
                             <dd class="italic">
                                 @foreach ($job->tags as $tag)
-                                    <a href="/jobs/tag/{{ $tag->name }}" class="mx-1 text-blue-400 hover:underline">
+                                    <a href="{{ route('jobs.tag', $tag->name) }}" class="mx-1 text-blue-400 hover:underline">
                                         {{ $tag->name }}
                                     </a>
                                     @if (! $loop->last)
@@ -48,4 +65,5 @@
     @else
         <div class="font-bold italic">Dololo.</div>
     @endif
+    @dump($jobs->count())
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Job;
+use App\Models\{Job, Tag, Employer};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -30,3 +30,31 @@ Route::get('/jobs/{id}', function ($id) {
 
     return view('jobs.view', ['job' => $job]);
 })->name('jobs.job.show');
+
+Route::get('/jobs/tag/{tag}', function ($tag) {
+    $tag = Tag::where('name', $tag)->first();
+    if ($tag === null) {
+        abort(404, 'We do not have that tag.');
+    }
+
+    $jobs = $tag->jobs()->with(['employer', 'tags'])->simplePaginate(5);
+    return view('jobs.index', [
+        'jobs' => $jobs,
+        'tag' => $tag
+    ]);
+})->name('jobs.tag');
+
+Route::get('/jobs/employer/{id}', function ($id) {
+    $employer = Employer::find($id);
+
+    if ($employer === null) {
+        abort(404, 'We do not have that employer.');
+    }
+
+    $jobs = $employer->jobs()->with(['tags'])->simplePaginate(5);
+
+    return view('jobs.index', [
+        'jobs' => $jobs,
+        'employer' => $employer
+    ]);
+})->name('jobs.employer');
